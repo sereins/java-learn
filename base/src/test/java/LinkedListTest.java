01a12c9418bd69617c2a755a745cf153e872e1b4
Jay <137034198@qq.com>
2022-04-13 23:22:49 +0800

    链表curd

--- base/src/test/java/LinkedListTest.java
+++ base/src/test/java/LinkedListTest.java
@@ -1,13 +1,27 @@
-import pers.sereins.algorithm.linked.LinkedList;
+import pers.sereins.algorithm.linked.MyLinkedList;
 
 public class LinkedListTest {
 
 
     public static void main(String[] args) {
 
-        LinkedList linkedList = new LinkedList();
+        MyLinkedList linkedList = new MyLinkedList();
 
-        System.out.println("获取指定下标元素" + linkedList.get(5));
+//        linkedList.addAtHead(1);
+//        linkedList.addAtTail(3);
+//
+//        linkedList.addAtIndex(1,2);
+//        linkedList.showLists();
+//        System.out.println();
+//        linkedList.deleteAtIndex(1);
+//        linkedList.showLists();
+////        int i = linkedList.get(1);
+
+
+        linkedList.addAtHead(1);
+        linkedList.deleteAtIndex(0);
+        linkedList.showLists();
+
 
     }
 }
